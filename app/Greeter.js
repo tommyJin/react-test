2981fd41a8a1d6d3cf68e24009d5ecba25041f1e
tommyjin <tommyjing0620@gmail.com>
2016-06-11 11:27:22 +0100

    test here

---
+++ app/Greeter.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tommy on 2016/6/10.
+ */
+import React, {Component} from 'react';
+import config from '../greet.json';
+
+class Greeter extends Component{
+    render(){
+        return(
+            <div >
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter
---
+++ app/main-compiled.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+import Greeter from './Greeter';
+
+render();
+
+var greeter = require('./Greeter.js');
+document.write("here we go!!!");
+document.getElementById('root').appendChild(greeter());
+
+//# sourceMappingURL=main-compiled.js.map
